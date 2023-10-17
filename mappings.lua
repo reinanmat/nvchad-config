8fb9e820315bfe25dd4f7d0114ad99069027c75f
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2023-10-17 10:19:45 -0300

    feat: added new mappings

--- mappings.lua
+++ mappings.lua
@@ -1,14 +1,35 @@
 local M = {}
 
+M.disabled = {
+	n = {
+		["jk"] = ""
+	}
+}
+
 M.abc = {
   n = {
+	["<leader>h"] = { "<cmd>:Stdheader <CR>" },
     ["x"] = { '"_x"' },
     ["<leader>if"] = { "=i{" },
+	["<leader>w"] = {"<cmd>:w <CR>"},
+	["<leader>q"] = {"<cmd>:qa <CR>"},
+	["<leader>tt"] = {
+		function()
+			require("base46").toggle_transparency()
+		end,
+		"toggle transparency",
+	},
+	["<S-t>"] = {
+		function()
+			require("base46").toggle_theme(0)
+		end,
+		"toggle transparency",
+	},
   },
   i = {
     ["<C-j>"] = { "<cmd>m +1 <CR>" },
     ["<C-k>"] = { "<cmd>m -2 <CR>" },
-    ["jk"] = { "<ESC>", opts = { nowait = true }}
+    ["jk"] = { "<ESC>"}
   },
   v = {
     ["<C-j>"] = { ":m '>+1<CR>gv=gv" },
