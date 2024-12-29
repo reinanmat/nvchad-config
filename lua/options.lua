4940017d3b12d2995d356fcdb93217428b1776d4
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-12-29 14:54:53 -0300

    fix: wsl2 clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -63,14 +63,14 @@ end
 
 if vim.fn.has("wsl") then
   vim.g.clipboard = {
-    name = "win_clipboard",
+    name = 'WslClipboard',
     copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
     },
     paste = {
-      ["+"] = "powershell.exe Get-Clipboard",
-      ["*"] = "powershell.exe Get-Clipboard",
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
     },
     cache_enabled = 0,
   }
